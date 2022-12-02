e201c56b260cef7a7e7374ad5a43fcec5c7c48b1
Ivan Day <ivanday@utexas.edu>
2022-12-02 09:55:20 -0600

    Create Reviews component and add to the app

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import Overview from './Overview/Overview.jsx';
 import QnA from './QnA/QnA.jsx';
 import RelatedItems from './RelatedItems/RelatedItems.jsx';
+import Reviews from './Reviews/Reviews.jsx';
 
 const root = createRoot(document.getElementById('root'));
 
@@ -13,6 +14,7 @@ function App() {
       <div><Overview /></div>
       <div><QnA /></div>
       <div><RelatedItems /></div>
+      <div><Reviews /></div>
     </div>
   );
 }
