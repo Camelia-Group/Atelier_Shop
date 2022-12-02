d12fc4cf273bfc2f4a67c4a86ac6003ad61e3a58
Ivan Day <ivanday@utexas.edu>
2022-12-02 09:50:49 -0600

    implement skeleton files for Overview and QnA widgets

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { createRoot } from "react-dom/client";
-const root = createRoot(document.getElementById("root"));
+import { createRoot } from 'react-dom/client';
+import Overview from './Overview/Overview.jsx';
+
+const root = createRoot(document.getElementById('root'));
 
 // Huzzah for jsx!
-const App = () => {
-  return <h1>Hello World</h1>
+function App() {
+  return (
+    <div><Overview /></div>
+  );
 }
 
 root.render(<App />);
