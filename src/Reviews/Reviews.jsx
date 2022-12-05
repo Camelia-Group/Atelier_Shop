8e8cb62aa631ecb18be8983d8010299eb05952c1
Ivan Day <ivanday@utexas.edu>
2022-12-05 16:02:18 -0600

    Revert "Created Template for Reviews and Ratings"
    This reverts commit 37ba5f90fbfcfde61468bafece82a92baeb45526.

--- src/Reviews/Reviews.jsx
+++ src/Reviews/Reviews.jsx
@@ -1,17 +1,5 @@
 import React from 'react';
-import Rating from './RatingBreakdown/Rating.jsx';
-import Tiles from './ReviewList/Tiles.jsx';
-import Factors from './Factors/Factors.jsx';
-
 
 export default function Reviews() {
-    return(
-
-      <>
-      <h1>Reviews</h1>
-      <Rating />
-      <Tiles />
-      <Factors />
-      </>
-    )
+  return <h1>Reviews</h1>;
 }
