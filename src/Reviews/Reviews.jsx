37ba5f90fbfcfde61468bafece82a92baeb45526
Stephen Chong <stephenc326@gmail.com>
2022-12-05 16:36:33 -0500

    Created Template for Reviews and Ratings

--- src/Reviews/Reviews.jsx
+++ src/Reviews/Reviews.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import Rating from './RatingBreakdown/Rating.jsx';
+import Tiles from './ReviewList/Tiles.jsx';
+import Factors from './Factors/Factors.jsx';
+
 
 export default function Reviews() {
-  return <h1>Reviews</h1>;
+    return(
+
+      <>
+      <h1>Reviews</h1>
+      <Rating />
+      <Tiles />
+      <Factors />
+      </>
+    )
 }
