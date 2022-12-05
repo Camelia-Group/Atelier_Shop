409f06080396a78c24737c3dd6cbce5bdca993d0
Ivan Day <ivanday@utexas.edu>
2022-12-05 17:33:51 -0600

    Add useEffect hook for api request, add props to Details, add css styling to put red outline on all elements

--- src/Overview/Details.jsx
+++ src/Overview/Details.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import './Overview.css';
 
-export default function Details() {
-  return <div>Details</div>;
+export default function Details({ product }) {
+  console.log(product);
+  return (
+    <div>
+      <h2>Details</h2>
+      <h3>{product.name}</h3>
+    </div>
+  );
 }
--- src/Overview/Overview.jsx
+++ src/Overview/Overview.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Details from './Details.jsx';
 
 const axios = require('axios');
@@ -6,18 +6,20 @@ const axios = require('axios');
 export default function Overview() {
   const [product, setProduct] = useState({});
 
-  axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products', {
-    headers: {
-      Authorization: process.env.API_KEY,
-    },
-  }).then((data) => {
-    console.log(data);
-  });
+  useEffect(() => {
+    axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products/37311', {
+      headers: {
+        Authorization: process.env.API_KEY,
+      },
+    }).then((response) => {
+      setProduct(response.data);
+    });
+  }, []);
 
   return (
     <div>
       <h1>Overview</h1>
-      <div><Details /></div>
+      <div><Details product={product} /></div>
     </div>
   );
 }
