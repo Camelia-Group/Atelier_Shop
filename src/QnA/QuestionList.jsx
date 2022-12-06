a746274464a16f72a94420f85c07cdd3fa40afe4
Brandon Vega <vegabrandon1250@gmail.com>
2022-12-06 17:58:04 -0500

    Undo last commint

--- src/QnA/QuestionList.jsx
+++ src/QnA/QuestionList.jsx
@@ -3,48 +3,48 @@ import PropTypes from 'prop-types';
 import Question from './Question.jsx';
 
 function QuestionList({ questions }) {
-  // const [questionRenderCount, setQuestionRenderCount] = useState(2);
+  const [questionRenderCount, setQuestionRenderCount] = useState(2);
 
-  // let questionsSorted = [];
-  // const questionKeys = Object.keys(questions);
-  // const checkSort = (currQuestions) => {
-  //   console.log('current Answers', currQuestions);
-  //   if (currQuestions.length === 1) {
-  //     return true;
-  //   }
-  //   for (let i = 0; i < currQuestions.length - 1; i += 1) {
-  //     if (currQuestions[i].helpfulness < currQuestions[i + 1].helpfulness) {
-  //       return false;
-  //     }
-  //   }
-  //   return true;
-  // };
-  // for (let i = 0; i < questionKeys.length; i += 1) {
-  //   questionsSorted.push(question.answers[answerKeys[i]]);
-  // }
+  let questionsSorted = [];
+  const questionKeys = Object.keys(questions);
+  const checkSort = (currQuestions) => {
+    console.log('current Answers', currQuestions);
+    if (currQuestions.length === 1) {
+      return true;
+    }
+    for (let i = 0; i < currQuestions.length - 1; i += 1) {
+      if (currQuestions[i].helpfulness < currQuestions[i + 1].helpfulness) {
+        return false;
+      }
+    }
+    return true;
+  };
+  for (let i = 0; i < questionKeys.length; i += 1) {
+    questionsSorted.push(question.answers[answerKeys[i]]);
+  }
 
-  // const sortQuestions = (currQuestions) => {
-  //   const returned = currQuestions;
-  //   console.log('RETURNED VAL', returned);
-  //   if (returned.length === 1) {
-  //     return returned;
-  //   }
-  //   for (let i = 0; i < returned.length - 1; i += 1) {
-  //     if (returned[i].helpfulness < returned[i + 1].helpfulness) {
-  //       const swapped = returned[i];
-  //       returned[i] = returned[i + 1];
-  //       returned[i + 1] = swapped;
-  //     }
-  //   }
-  //   if (checkSort(returned)) {
-  //     return returned;
-  //   }
-  //   return sortQuestions(returned);
-  // };
-  // console.log('PASSED SORTED', questionsSorted);
-  // questionsSorted = sortQuestions(questionsSorted);
+  const sortQuestions = (currQuestions) => {
+    const returned = currQuestions;
+    console.log('RETURNED VAL', returned);
+    if (returned.length === 1) {
+      return returned;
+    }
+    for (let i = 0; i < returned.length - 1; i += 1) {
+      if (returned[i].helpfulness < returned[i + 1].helpfulness) {
+        const swapped = returned[i];
+        returned[i] = returned[i + 1];
+        returned[i + 1] = swapped;
+      }
+    }
+    if (checkSort(returned)) {
+      return returned;
+    }
+    return sortQuestions(returned);
+  };
+  console.log('PASSED SORTED', questionsSorted);
+  questionsSorted = sortQuestions(questionsSorted);
 
-  // const renderedQuestions = questionsSorted.slice(0, questionRenderCount);
+  const renderedQuestions = questionsSorted.slice(0, questionRenderCount);
   return (
     <div className="question-list">
       {
