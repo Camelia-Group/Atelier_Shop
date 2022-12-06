7491da522c88213f82e7a74ad915fd99f7601f82
Stephen Chong <stephenc326@gmail.com>
2022-12-06 16:28:35 -0500

    Updated reviews and ratings

--- webpack.config.js
+++ webpack.config.js
@@ -22,20 +22,23 @@ module.exports = {
       },
     ],
   },
-  // [devtool] this is an additional source map that will let the browser know what files are running our code.
-  // Helps with error tracing. Without it we will not know where our errors are coming from because it will state that everything inside the bundle file.
+
   devtool: 'eval-cheap-module-source-map',
-  // [devServer] configuration for the live server including port
+
   devServer: {
-    // [static] config for how what to serve
+
     static: {
       directory: path.join(__dirname, 'public'),
     },
     compress: true,
-    // [port] what port on our local machine to run the dev server
+
     port: 3000,
   },
   plugins: [
     new Dotenv(),
   ],
+  watchOptions: {
+    poll: true,
+    ignored: /node_modules/,
+  },
 };
