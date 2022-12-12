49d98ef66b35543d550d2c1030351489849df4ce
Ivan Day <ivanday@utexas.edu>
2022-12-12 15:13:04 -0600

    Add forward and back arrows to the thumbnail photo slider

--- src/Overview/ImageGallery.jsx
+++ src/Overview/ImageGallery.jsx
@@ -9,11 +9,27 @@ export default function ImageGallery({ selectedImage, setSelectedImage, selected
     if (selectedStyle.photos !== undefined) {
       setSelectedImage(selectedStyle.photos[photoIndex].url);
     }
-  }, [selectedStyle]);
+  }, [selectedStyle, photoIndex]);
 
   const handleThumbnailClick = (url, index) => {
     setPhotoIndex(index);
     setSelectedImage(url);
+  };
+
+  const handleNextClick = () => {
+    if (photoIndex >= selectedStyle.photos.length - 1) {
+      setPhotoIndex(0);
+    } else {
+      setPhotoIndex(photoIndex + 1);
+    }
+  };
+
+  const handlePreviousClick = () => {
+    if (photoIndex === 0) {
+      setPhotoIndex(selectedStyle.photos.length - 1);
+    } else {
+      setPhotoIndex(photoIndex - 1);
+    }
   };
 
   if (selectedStyle.photos !== undefined) {
@@ -23,9 +39,11 @@ export default function ImageGallery({ selectedImage, setSelectedImage, selected
           <img src={selectedImage} alt="" style={{ 'max-width': '100%', 'max-height': '100%' }} />
         </div>
         <div className="image-thumbnail-nav">
+          <input type="button" onClick={() => handlePreviousClick()} value="previous" />
           {selectedStyle.photos.map((image, index) => (
             <img src={image.thumbnail_url} onClick={()=>handleThumbnailClick(image.url, index)} className="image-thumbnail" />
           ))}
+          <input type="button" onClick={() => handleNextClick()} value="next" />
         </div>
       </div>
     );
